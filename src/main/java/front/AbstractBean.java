14f6ec2bb3a1e2d7e469eee9448aad2c904eabdc
21ruedelyon <jerome.michel.serre@gmail.com>
2023-07-06 07:42:35 +0200

    Create AbstractBean.java
    Tous les beans devront hériter de AbstractBean pour d'une part bénéficier de la serialization et d'autre part pour avoir accès aux objets factorisés

---
+++ src/main/java/front/AbstractBean.java
@@ -0,0 +1,15 @@
+package front;
+
+import java.io.Serializable;
+
+public abstract class AbstractBean implements Serializable {
+
+	private static final long serialVersionUID = 4559390409343722283L;
+	
+	@Override
+	public String toString() {
+		String result = "";
+		return result;
+	}
+
+}
